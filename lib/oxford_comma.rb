f6d4442a2bb59bebc94dcb17179889a3186271ea
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-14 18:36:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-    "#{[array]}"
+    "#{array[0..-2].join(",")}, and #{array.last}"
   elsif array.length == 1
     array.join
   end
